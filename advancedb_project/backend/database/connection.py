dc801dfcc4fba455d90cfde3bd433c1337cfc026
Erick De Belen <erickdebelen22@gmail.com>
2025-05-24 11:02:42 +0800

    trying to deploy

--- advancedb_project/backend/database/connection.py
+++ advancedb_project/backend/database/connection.py
@@ -1,13 +1,14 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='1025',
-            database='LabaRide_DB'
+            host=os.getenv('DB_HOST', 'localhost'),
+            user=os.getenv('DB_USER', 'root'),
+            password=os.getenv('DB_PASSWORD', '1025'),
+            database=os.getenv('DB_NAME', 'LabaRide_DB')
         )
         if connection.is_connected():
             print("Successfully connected to MySQL database")
